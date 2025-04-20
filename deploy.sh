a91d2a8da1425e532862d5b8508fb68a3557de06
Gajendra Nishad <75714258+gajjug004@users.noreply.github.com>
2025-04-20 18:11:34 +0530

    modified deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,12 +6,13 @@ GITHUB_REPO_URL="https://github.com/gajjug004/test-deploy.git"
 PROJECT_DIR="/home/ubuntu/$PROJECT_NAME"
 VENV_DIR="$PROJECT_DIR/venv"
 GUNICORN_SOCK="/run/gunicorn.sock"
-DJANGO_SETTINGS_MODULE="backend"
-ALLOWED_HOSTS=None
+DJANGO_MODULE="backend"  # Folder where settings.py and wsgi.py are located
+DJANGO_SETTINGS_MODULE="$DJANGO_MODULE.settings"
+ALLOWED_HOSTS="localhost"
 
 # Clone the GitHub repository
 if [ ! -d "$PROJECT_DIR" ]; then
-    git clone $GITHUB_REPO_URL
+    git clone $GITHUB_REPO_URL $PROJECT_DIR
 else
     echo "Directory $PROJECT_DIR already exists. Pulling latest changes."
     cd $PROJECT_DIR
@@ -30,9 +31,11 @@ fi
 source $VENV_DIR/bin/activate
 
 # Install required Python packages
+pip install --upgrade pip
 pip install -r requirements.txt
 
 # Run database migrations
+python manage.py makemigrations
 python manage.py migrate
 
 # Collect static files
@@ -44,7 +47,7 @@ cat <<EOL | sudo tee /etc/systemd/system/gunicorn.socket
 Description=gunicorn socket
 
 [Socket]
-ListenStream=/run/gunicorn.sock
+ListenStream=$GUNICORN_SOCK
 
 [Install]
 WantedBy=sockets.target
@@ -61,26 +64,24 @@ After=network.target
 User=ubuntu
 Group=www-data
 WorkingDirectory=$PROJECT_DIR
-Environment="PATH=$VENV_DIR/bin"
-ExecStart=$VENV_DIR/bin/gunicorn --access-logfile - --workers 3 --bind unix:$GUNICORN_SOCK $DJANGO_SETTINGS_MODULE.wsgi:application
+Environment="VIRTUAL_ENV=$VENV_DIR"
+Environment="PATH=$VENV_DIR/bin:/usr/bin"
+ExecStart=$VENV_DIR/bin/gunicorn --access-logfile - --workers 3 --bind unix:$GUNICORN_SOCK $DJANGO_MODULE.wsgi:application
 
 [Install]
 WantedBy=multi-user.target
 EOL
 
 # Start and enable Gunicorn service
-sudo systemctl start gunicorn
-sudo systemctl enable gunicorn
-
-# Set ALLOWED_HOSTS to _ if it's empty or set to "none"
-if [ -z "$ALLOWED_HOSTS" ] || [ "$ALLOWED_HOSTS" = "None" ]; then
-  ALLOWED_HOSTS="_"
-fi
+sudo systemctl daemon-reexec
+sudo systemctl daemon-reload
+sudo systemctl enable --now gunicorn.socket
+sudo systemctl restart gunicorn
 
 # Create an Nginx server block configuration
 cat <<EOL | sudo tee /etc/nginx/sites-available/$PROJECT_NAME
 server {
-    listen 80 default_server;
+    listen 80;
     server_name $ALLOWED_HOSTS;
 
     location = /favicon.ico { access_log off; log_not_found off; }
@@ -96,15 +97,11 @@ server {
 EOL
 
 # Enable the Nginx server block
-sudo ln -s /etc/nginx/sites-available/$PROJECT_NAME /etc/nginx/sites-enabled
-sudo nginx -t
+if [ ! -L "/etc/nginx/sites-enabled/$PROJECT_NAME" ]; then
+    sudo ln -s /etc/nginx/sites-available/$PROJECT_NAME /etc/nginx/sites-enabled/
+fi
 
-# Restart Nginx and gunicorn to apply changes
-sudo systemctl restart nginx
-
-sudo service gunicorn restart
-
-sudo service nginx restart
-
+# Test Nginx configuration and restart services
+sudo nginx -t && sudo systemctl restart nginx
 
 echo "Deployment of $PROJECT_NAME completed successfully!"
